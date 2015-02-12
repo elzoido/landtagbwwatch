317372f29895c618bac048e02e33f9bd513e6d6a
Sebastian Sproesser <elzoido@elbloggo.de>
2015-02-12 16:01:58 +0100

    Added JSON output. Started to prepare for CGI use.

--- getdrucksachen.pl
+++ getdrucksachen.pl
@@ -4,20 +4,24 @@ use warnings;
 use strict;
 
 use Time::Piece;
-
 use LWP::Simple qw/get/;
-
 use Web::Scraper;
-
 use utf8;
-
 use Encode qw/decode encode/;
+use JSON;
 
-my $baseurl = 'http://www.landtag-bw.de/';
+use CGI qw/:standard/;
 
-my $basesearch = $baseurl . 'cms/render/live/de/sites/LTBW/home/dokumente/die-initiativen/drucksachen/contentBoxes/suche-drucksachen.html';
+# CGI-Parameters
+#   searchYear (YYYY/YY) default: current year
+#   searchMonth (M/MM) default: current month
+#   outputFormat (json,prettyjson,csv) default: prettyjson
 
-# Parameters:
+my $baseurl = 'http://www.landtag-bw.de';
+
+my $basesearch = $baseurl . '/cms/render/live/de/sites/LTBW/home/dokumente/die-initiativen/drucksachen/contentBoxes/suche-drucksachen.html';
+
+# Parameters for Interface:
 #   searchYear (YYYY)
 #   searchMonth (MM)
 
@@ -32,12 +36,12 @@ if ($month == 1) {
 	$lastmonth = $month - 1;
 }
 
+$lastmonth = '0' . $lastmonth if (length($lastmonth) == 1);
+
 my $content = get($basesearch.'?searchYear='.$year.'&searchMonth='.$month);
 
 my $scraper = scraper {
    process "div.result", "drucksachen[]" => scraper {
-       # And, in that array, pull in the elementy with the class
-       # "entry-content", "entry-date" and the link
        process ".kategorie > p", meta => 'TEXT';
        process 'h3 > a', link => '@href';
        process 'h3 > a', title => 'TEXT';
@@ -48,40 +52,68 @@ my $ds = $scraper->scrape($content);
 
 my $drucksachen;
 
-print "drucksache,datum,link,titel\n";
+my $items;
 
-for my $item (sort sortDate @{$ds->{drucksachen}}) {
+for my $item (sort sortMetaDate @{$ds->{drucksachen}}) {
 	my $drucksache_id;
 	my $date;
 	my $link;
 	my $title;
 	($drucksache_id, $date) = (encode('UTF-8',$item->{meta}) =~ /(\d+\/\d+).*(\d\d\.\d\d\.\d\d\d\d)/);
-	$link = $baseurl . $item->{link};
+	$link = $baseurl . (substr($item->{link},0,1) eq '/' ? '' : '/') . $item->{link};
 	$title = encode('UTF-8', $item->{title});
-	1;
-	print $drucksache_id.",".$date.",\"".$link."\",\"".$title."\"\n";
+	$items->{$drucksache_id} = {date => $date,
+								link => $link,
+								title => $title};
 }
 
 $content = get($basesearch.'?searchYear='.$lastyear.'&searchMonth='.$lastmonth);
 
 $ds = $scraper->scrape($content);
-for my $item (sort sortDate @{$ds->{drucksachen}}) {
+for my $item (sort sortMetaDate @{$ds->{drucksachen}}) {
 	my $drucksache_id;
 	my $date;
 	my $link;
 	my $title;
 	($drucksache_id, $date) = (encode('UTF-8',$item->{meta}) =~ /(\d+\/\d+).*(\d\d\.\d\d\.\d\d\d\d)/);
-	$link = $baseurl . $item->{link};
+	$link = $baseurl . (substr($item->{link},0,1) eq '/' ? '' : '/') . $item->{link};
 	$title = encode('UTF-8', $item->{title});
-	1;
-	print $drucksache_id.",".$date.",\"".$link."\",\"".$title."\"\n";
+	$items->{$drucksache_id} = {date => $date,
+								link => $link,
+								title => $title};
+}
+
+#outputCSV($items);
+outputJSON($items);
+
+sub outputCSV {
+	my $items = shift;
+	print "drucksache,datum,link,titel\n";
+
+	for my $id (sort sortRefDate keys %$items) {
+		print $id.",".$items->{$id}->{date}.",\"".$items->{$id}->{link}."\",\"".$items->{$id}->{title}."\"\n";
+	}
+}
+
+sub outputJSON {
+	my $items = shift;
+	print to_json($items, {pretty => 1});
+}
+
+sub sortMetaDate {
+	return sortDate($b->{meta},$a->{meta});
 }
 
 sub sortDate {
-	my ($a_d, $a_m, $a_y) = ($a->{meta} =~ /(\d\d)\.(\d\d)\.(\d\d\d\d)/);
-	my ($b_d, $b_m, $b_y) = ($b->{meta} =~ /(\d\d)\.(\d\d)\.(\d\d\d\d)/);
+	my ($a, $b) = (shift, shift);
+	my ($a_d, $a_m, $a_y) = ($a =~ /(\d\d)\.(\d\d)\.(\d\d\d\d)/);
+	my ($b_d, $b_m, $b_y) = ($b =~ /(\d\d)\.(\d\d)\.(\d\d\d\d)/);
 	return $b_y <=> $a_y
 		|| $b_m <=> $a_m
 		|| $b_d <=> $a_d;
 	
+}
+
+sub sortRefDate {
+	return sortDate($items->{$a}->{date}, $items->{$b}->{date});
 }
