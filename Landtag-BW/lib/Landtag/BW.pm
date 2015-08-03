edc1d4091bfb8ebb95ed99e67ea3070a80c439e7
Sebastian Sproesser <elzoido@elbloggo.de>
2015-08-03 11:33:19 +0200

    Other debugging

--- Landtag-BW/lib/Landtag/BW.pm
+++ Landtag-BW/lib/Landtag/BW.pm
@@ -355,16 +355,16 @@ get '/kategorien/:kategorie_id' => sub {
 		my $ds_res = $ds_sth->fetchall_arrayref({});
 		my $init_res = $init_sth->fetchall_arrayref({});
 		
-		$db_result->{$id}->{initiativen} = $init_res;
-		$db_result->{$id}->{drucksachen} = $ds_res;
+		$result->{$id}->{initiativen} = $init_res;
+		$result->{$id}->{drucksachen} = $ds_res;
 	}
 
-	debug($db_result);
+	debug($result);
 
 	# show matching kleine Anfragen
 	template 'kategorie', {
 		name => $kategorie->{name},
-		suchbegriffe => $db_result,
+		suchbegriffe => $result,
 		neu => 1,
 	}
 };
